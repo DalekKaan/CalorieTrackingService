4a9e14407613d12792b75ec33c2a6189a7bfbf94
Alexey Seleznev <rib@live.ru>
2025-03-25 03:33:36 +0300

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calorie tracking service
+This is testing task for 1221Systems job interview.
---
+++ src/main/java/ru/r1b/calorietrackingservice/CalorieTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.r1b.calorietrackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieTrackingServiceApplication.class, args);
+    }
+
+}
