3086ec1f9fadbcc6f8b9a58f590d1e0b8a001261
Alexey Seleznev <rib@live.ru>
2025-03-26 22:52:17 +0300

    added named params

--- src/main/java/ru/r1b/calorietrackingservice/query/person/DailyReportQuery.java
+++ src/main/java/ru/r1b/calorietrackingservice/query/person/DailyReportQuery.java
@@ -1,6 +1,8 @@
 package ru.r1b.calorietrackingservice.query.person;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.r1b.calorietrackingservice.scheme.userstatistics.DailyReport;
 
@@ -9,31 +11,30 @@ import java.util.UUID;
 
 @Repository
 public class DailyReportQuery {
-    JdbcTemplate jdbcTemplate;
+    NamedParameterJdbcTemplate jdbcTemplate;
 
-    public DailyReportQuery(JdbcTemplate jdbcTemplate) {
+    public DailyReportQuery(NamedParameterJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public DailyReport getData(UUID personId, LocalDate date) {
-        // todo: use named params
         String sql = "SELECT sum(d.caloric_content) AS caloriesReceived, count(DISTINCT e.*) AS eating\n" +
                 "FROM eating e\n" +
                 "         INNER JOIN dish_in_eating die ON e.id = die.eating_id\n" +
                 "         INNER JOIN dish d on die.dish_id = d.id\n" +
-                "WHERE person_id = ? \n" +
-                "  AND date_time BETWEEN ? AND ? \n" +
+                "WHERE person_id = :personId \n" +
+                "  AND date(date_time) = :date \n" +
                 "GROUP BY person_id, date(date_time)";
         var result = jdbcTemplate.query(
                 sql,
+                new MapSqlParameterSource()
+                        .addValue("personId", personId)
+                        .addValue("date", date),
                 (resultSet, i) -> {
                     double caloriesReceived = resultSet.getDouble("caloriesReceived");
                     int eating = resultSet.getInt("eating");
                     return new DailyReport(caloriesReceived, eating);
-                },
-                personId,
-                date.atStartOfDay(),
-                date.plusDays(1).atStartOfDay()
+                }
         );
 
         return result.isEmpty() ? null : result.get(0);
