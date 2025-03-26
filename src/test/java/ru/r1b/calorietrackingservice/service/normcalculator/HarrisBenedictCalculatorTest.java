173f9d56ecb1e7fbae04a63a75da524dc063d998
Alexey Seleznev <rib@live.ru>
2025-03-26 05:27:02 +0300

    set up unit test

--- src/test/java/ru/r1b/calorietrackingservice/service/normcalculator/HarrisBenedictCalculatorTest.java
+++ src/test/java/ru/r1b/calorietrackingservice/service/normcalculator/HarrisBenedictCalculatorTest.java
@@ -1,33 +1,40 @@
 package ru.r1b.calorietrackingservice.service.normcalculator;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import ru.r1b.calorietrackingservice.enumerate.Gender;
 import ru.r1b.calorietrackingservice.enumerate.Purpose;
 import ru.r1b.calorietrackingservice.model.Person;
 import ru.r1b.calorietrackingservice.service.PersonService;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HarrisBenedictCalculatorTest {
 
-    @Test
-    @Disabled // todo: remove when ready
-    void calculate() {
+    @ParameterizedTest
+    @MethodSource("provideUsersForCalculate")
+    void calculate(Person person, int expected) {
 
         var calculator = new HarrisBenedictCalculator(new PersonService());
-        // todo: use @MethodValues
-        var user = new Person(
-                "John Smith",
-                "john.smith@yandex.ru",
-                LocalDate.of(1990,1,1),
-                Gender.MALE,
-                100,
-                180,
-                Purpose.MAINT
+        assertEquals(expected, calculator.calculate(person));
+    }
+
+    private static Stream<Arguments> provideUsersForCalculate() {
+        return Stream.of(
+                Arguments.of(new Person(
+                                "John Smith",
+                                "john.smith@yandex.ru",
+                                LocalDate.of(1990, 1, 1),
+                                Gender.MALE,
+                                100,
+                                180,
+                                Purpose.MAINT
+                        ),
+                        2285)
         );
-        assertEquals(2285, calculator.calculate(user));
     }
 }
