5e6206ef1e87dc6497dcd15d13b4b32b743efc35
Alexey Seleznev <rib@live.ru>
2025-03-26 18:57:11 +0300

    trying to use query

--- src/main/java/ru/r1b/calorietrackingservice/query/person/DailyReportQuery.java
+++ src/main/java/ru/r1b/calorietrackingservice/query/person/DailyReportQuery.java
@@ -1,38 +1,41 @@
 package ru.r1b.calorietrackingservice.query.person;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.r1b.calorietrackingservice.scheme.userstatistics.DailyReport;
 
-import java.sql.Types;
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Repository
 public class DailyReportQuery {
-    NamedParameterJdbcTemplate jdbcTemplate;
+    JdbcTemplate jdbcTemplate;
 
-    public DailyReportQuery(NamedParameterJdbcTemplate jdbcTemplate) {
+    public DailyReportQuery(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public DailyReport getData(UUID personId, LocalDate date) {
+        // todo: use named params
         String sql = "SELECT sum(d.caloric_content) AS caloriesReceived, count(DISTINCT e.*) AS eating\n" +
                 "FROM eating e\n" +
                 "         INNER JOIN dish_in_eating die ON e.id = die.eating_id\n" +
                 "         INNER JOIN dish d on die.dish_id = d.id\n" +
-                "WHERE person_id = :personId\n" +
-                "  AND date(date_time) = :date\n" +
+                "WHERE person_id = ? \n" +
+                "  AND date_time BETWEEN ? AND ? \n" +
                 "GROUP BY person_id, date(date_time)";
-        return jdbcTemplate.query(
+        var result = jdbcTemplate.query(
                 sql,
-                new MapSqlParameterSource()
-                        .addValue("personId", personId)
-                        .addValue("date", date),
-                new BeanPropertyRowMapper<DailyReport>(DailyReport.class)
-        ).get(0);
+                (resultSet, i) -> {
+                    double caloriesReceived = resultSet.getDouble("caloriesReceived");
+                    int eating = resultSet.getInt("eating");
+                    return new DailyReport(caloriesReceived, eating);
+                },
+                personId,
+                date.atStartOfDay(),
+                date.plusDays(1).atStartOfDay()
+        );
+
+        return result.isEmpty() ? null : result.get(0);
     }
 }
