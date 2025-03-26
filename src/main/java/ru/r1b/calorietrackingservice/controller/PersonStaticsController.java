08c20026791f8486bb582c969fb522466de79fe9
Alexey Seleznev <rib@live.ru>
2025-03-26 18:33:54 +0300

    trying to use query

--- src/main/java/ru/r1b/calorietrackingservice/controller/PersonStaticsController.java
+++ src/main/java/ru/r1b/calorietrackingservice/controller/PersonStaticsController.java
@@ -1,22 +1,37 @@
 package ru.r1b.calorietrackingservice.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.r1b.calorietrackingservice.scheme.personstatistics.CheckLimit;
 import ru.r1b.calorietrackingservice.scheme.userstatistics.DailyReport;
 import ru.r1b.calorietrackingservice.scheme.userstatistics.EatingHistory;
+import ru.r1b.calorietrackingservice.service.PersonService;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.UUID;
 
 @RestController()
 @RequestMapping("/person")
 public class PersonStaticsController {
+    private final PersonService personService;
+    PersonService service;
+
+    public PersonStaticsController(PersonService service, PersonService personService) {
+        this.service = service;
+        this.personService = personService;
+    }
+
     @GetMapping("/daily-report")
-    public DailyReport getDailyReport(UUID userId) {
-        // todo: to be implemented
-        return new DailyReport(0.0,0);
+    public DailyReport getDailyReport(
+            @RequestParam UUID personId,
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ) {
+        return personService.getDailyReport(personId, date);
     }
 
     @GetMapping("/check-limit")
--- src/main/java/ru/r1b/calorietrackingservice/service/PersonService.java
+++ src/main/java/ru/r1b/calorietrackingservice/service/PersonService.java
@@ -1,11 +1,29 @@
 package ru.r1b.calorietrackingservice.service;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.r1b.calorietrackingservice.model.Person;
+import ru.r1b.calorietrackingservice.query.person.DailyReportQuery;
+import ru.r1b.calorietrackingservice.scheme.userstatistics.DailyReport;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @Service
 public class PersonService {
+    DailyReportQuery dailyReportQuery;
+
+    public PersonService(DailyReportQuery dailyReportQuery) {
+        this.dailyReportQuery = dailyReportQuery;
+    }
+
+    public DailyReport getDailyReport(UUID personId, LocalDate date) {
+        return dailyReportQuery.getData(personId, date);
+    }
+
+    public DailyReport getDailyReport(Person person, LocalDate date) {
+        return dailyReportQuery.getData(person.getId(), date);
+    }
+
+
 }
