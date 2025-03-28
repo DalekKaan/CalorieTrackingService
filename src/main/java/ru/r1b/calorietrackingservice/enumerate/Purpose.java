7cca345c3770688689d72eae9b39e6e6ebd589ad
Alexey Seleznev <rib@live.ru>
2025-03-29 00:34:31 +0300

    cleaned up enums

--- src/main/java/ru/r1b/calorietrackingservice/enumerate/Purpose.java
+++ src/main/java/ru/r1b/calorietrackingservice/enumerate/Purpose.java
@@ -1,13 +1,7 @@
 package ru.r1b.calorietrackingservice.enumerate;
 
 public enum Purpose {
-    LOSS("loss"),
-    GAIN("gain"),
-    MAINT("maint");
-
-    public final String val;
-
-    Purpose(String val) {
-        this.val = val;
-    }
+    LOSS(),
+    GAIN(),
+    MAINT()
 }
