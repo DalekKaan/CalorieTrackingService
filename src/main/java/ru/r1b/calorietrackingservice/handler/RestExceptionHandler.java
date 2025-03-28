9f4c551cbbd984aaa7b7ab1f953b9f8dd8a2d4bb
Alexey Seleznev <rib@live.ru>
2025-03-28 17:27:02 +0300

    added TypeMismatch handler

--- src/main/java/ru/r1b/calorietrackingservice/handler/RestExceptionHandler.java
+++ src/main/java/ru/r1b/calorietrackingservice/handler/RestExceptionHandler.java
@@ -1,12 +1,16 @@
 package ru.r1b.calorietrackingservice.handler;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.r1b.calorietrackingservice.scheme.error.RestError;
@@ -18,10 +22,22 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+
+
     @ExceptionHandler(ResponseStatusException.class)
     protected ResponseEntity<Object> handleResponseStatusException(
             ResponseStatusException ex) {
         RestError apiError = new RestError((HttpStatus) ex.getStatusCode(), ex.getReason(), ex);
+        return buildResponseEntity(apiError);
+    }
+
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Object[] args = new Object[]{ex.getPropertyName(), ex.getValue()};
+        String var10000 = String.valueOf(args[0]);
+        String defaultDetail = "Failed to convert '" + var10000 + "' with value: '" + args[1] + "'";
+        RestError apiError = new RestError(BAD_REQUEST, defaultDetail, ex);
         return buildResponseEntity(apiError);
     }
 
