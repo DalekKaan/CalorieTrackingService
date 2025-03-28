8ace6530819ea954d0b524a8423c43d00d55ba4a
Alexey Seleznev <rib@live.ru>
2025-03-29 00:41:59 +0300

    finished clean up

--- src/main/java/ru/r1b/calorietrackingservice/model/ResourceEntity.java
+++ src/main/java/ru/r1b/calorietrackingservice/model/ResourceEntity.java
@@ -4,7 +4,7 @@ package ru.r1b.calorietrackingservice.model;
 import java.util.UUID;
 
 /**
- * Resource entity to be used in CommonResourceController
+ * Resource entity to be used in <code>CommonResourceController</code>
  */
 public interface ResourceEntity {
     UUID getId();
